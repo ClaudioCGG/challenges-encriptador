f141d4bcfed87fa56c4b3bab1c9985c2ae4a22fb
Claudio Guirado <claudioguirado@hotmail.com>
2024-03-19 17:03:25 -0300

    funcionesOk

--- validaciones.js
+++ validaciones.js
@@ -15,3 +15,69 @@ textarea.addEventListener("input", function () {
     textarea.value = textoIngresado.replace(/[^a-zA-Z\s]/g, ""); // Eliminar caracteres especiales
   }
 });
+
+function encriptarTexto() {
+  var mensaje = document.getElementById("textoEncriptar").value;
+  console.log(mensaje);
+
+  if (mensaje === "") {
+    // Mostrar mensaje de error y la imagen
+    document.querySelector(".mensaje-resultado").textContent =
+      "Ningún mensaje fue encontrado";
+  } else {
+    var mensajeNuevo = "";
+
+    for (var i = 0; i < mensaje.length; i++) {
+      switch (mensaje[i]) {
+        case "a":
+          mensajeNuevo += "ai";
+          break;
+        case "e":
+          mensajeNuevo += "enter";
+          break;
+        case "i":
+          mensajeNuevo += "imes";
+          break;
+        case "o":
+          mensajeNuevo += "ober";
+          break;
+        case "u":
+          mensajeNuevo += "ufat";
+          break;
+        default:
+          mensajeNuevo += mensaje[i];
+      }
+    }
+
+    // Ocultar mensaje de error y la imagen
+    document.getElementById("imgCifrar").style.display = "none";
+    document.querySelector(".mensaje-resultado").textContent = mensajeNuevo;
+  }
+}
+
+function desencryptarTexto() {
+  var mensaje = document.getElementById("textoEncriptar").value;
+
+  if (mensaje === "") {
+    // Mostrar mensaje de error y la imagen
+    document.querySelector(".mensaje-resultado").textContent =
+      "Ningún mensaje fue encontrado";
+  } else {
+    var mensajeNuevo = mensaje
+      .replace(/ai/g, "a")
+      .replace(/enter/g, "e")
+      .replace(/imes/g, "i")
+      .replace(/ober/g, "o")
+      .replace(/ufat/g, "u");
+
+    // Ocultar mensaje de error y la imagen
+    document.getElementById("imgCifrar").style.display = "none";
+    document.querySelector(".mensaje-resultado").textContent = mensajeNuevo;
+  }
+}
+
+function copyTexto() {
+  var textoCopiar = document.querySelector(".mensaje-resultado").textContent;
+
+  document.getElementById("textoEncriptar").value = textoCopiar;
+}
